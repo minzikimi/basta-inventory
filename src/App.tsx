19199b3d7c0c00c67f92f280564913c11e010bff
minzikimi <minji.kim@hyperisland.se>
2026-01-03 01:17:38 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Basta bar inventory dashboard</h1>
+    </div>
+  );
+}
+
+export default App;
